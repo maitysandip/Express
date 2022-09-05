d9f173942f13bbc7b5980bb3f61ad24c3548dbee
Sandip <sandipmaity345@gmail.com>
2022-09-05 16:40:51 +0530

    updated text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello from Node App </h1>")
+    res.send("<h1>Hello from Node App with Nginx Reverse Proxy</h1>")
 })
 // define routes here..
 
