24d70b9be9fcd5f66b4adeec29b8cca00d2cdef0
Sandip <sandipmaity345@gmail.com>
2022-09-05 15:23:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello from Node App </h1>")
+})
+// define routes here..
+
+var server = app.listen(5000, () => {
+    console.log('Node server is running..');
+});
